14379202308eb10341933e8c57abd5564fa4d6cb
dbartell <bartelldevyn@gmail.com>
2015-11-13 20:55:17 -0700

    howdy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 var mongoose = require('mongoose');
 var session = require('express-session');
 // herlsdofajpsdofiajdfs;
-var passportCode = require('./Passport.js');
+var passportCode = require('./Server/Passport.js');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
